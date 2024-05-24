078324ec6574de3a6946150cd7a9c2734d189c1d
cdroot01 <cdroot01@gmail.com>
2024-05-24 15:21:05 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cdroot01/go01
+
+go 1.22.3
+
+require github.com/cdroot01/go837 v1.1.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cdroot01/go837/greetings"
+)
+
+func main() {
+	fmt.Println("V0 : go01 work")
+	
+	fmt.Println(greetings.Hello("Cong"))
+}
